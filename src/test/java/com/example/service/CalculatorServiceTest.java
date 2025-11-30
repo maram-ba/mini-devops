de59d0c5e921ecb8f9247eec44127a05e192c315
root <root@DESKTOP-J6QR50Q.localdomain>
2025-11-30 01:44:34 +0100

    Ajout des fonctions modulo et power avec tests unitaires

---
+++ src/test/java/com/example/service/CalculatorServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.service;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorServiceTest {
+
+    @Test
+    public void testAdd() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(2, calc.divide(10, 5));
+    }
+
+    @Test
+    public void testModulo() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(1, calc.modulo(10, 3));
+    }
+
+    @Test
+    public void testPower() {
+        CalculatorService calc = new CalculatorService();
+        assertEquals(8, calc.power(2, 3)); // 2^3 = 8
+    }
+}
